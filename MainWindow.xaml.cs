4021d67a9998e73525394fee32b2aa4b4b4c4e05
elcommendante <65852148+elcommendante@users.noreply.github.com>
2020-10-02 09:33:24 +0100

    Initial commit

---
+++ MainWindow.xaml.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace Qontrol
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+        private void BtnClickMainPage(object sender, RoutedEventArgs e)
+        {
+            Main.Content = new MainPage();
+        }
+
+        private void BtnClickPageTasks(object sender, RoutedEventArgs e)
+        {
+            Main.Content = new PageTasks();
+        }
+
+        private void BtnClickPageCalculator(object sender, RoutedEventArgs e)
+        {
+            Main.Content = new PageCalculator();
+        }
+
+        private void BtnClickPageNotes(object sender, RoutedEventArgs e)
+        {
+            Main.Content = new PageNotes();
+        }
+
+        private void BtnClickPageCalendar(object sender, RoutedEventArgs e)
+        {
+            Main.Content = new PageCalendar();
+        }
+    }
+}
---
+++ README.md
@@ -0,0 +1,2 @@
+# Qontrol
+ 
